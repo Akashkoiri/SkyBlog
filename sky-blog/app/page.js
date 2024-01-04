79e853a892bca6688ff298908fcf86b26f23aeaf
Akash_koiri <akk27724@gmail.com>
2024-01-04 15:17:31 +0000

    Modified Home page

---
+++ sky-blog/app/page.js
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import Head from 'next/head'
+import styles from '../styles/page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Head>
+        <title>Sky Blog | Home Page</title>
+      </Head>
+      
+      <h1>SKY BLOG</h1>
+
+      <div className={styles.grid}>
+        <div className={styles.card}>
+          <h2>Docs <span>-&gt;</span></h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </div>
+        <div className={styles.card}>
+          <h2>Learn <span>-&gt;</span></h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </div>
+        <div className={styles.card}>
+          <h2>Templates <span>-&gt;</span></h2>
+          <p>Explore starter templates for Next.js.</p>
+        </div>
+        <div className={styles.card}>
+          <h2>Deploy <span>-&gt;</span></h2>
+          <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
+        </div>
+        
+      </div>
+    </main>
+  )
+}
